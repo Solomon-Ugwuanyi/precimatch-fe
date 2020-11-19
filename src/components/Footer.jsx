b267d52f40247a567baee70804707603828fe43c
Solomon <solomon6u@yahoo.com>
2020-11-19 01:19:08 +0100

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,10 +10,6 @@ export default function Footer() {
                     <p className="float-right">
                         <a href="#">Back to top</a>
                     </p>
-                    <ArrowRight />
-
-
-
                     <p>PreciMatch is a &copy; PreciGroup. </p>
                     <p>New to Bootstrap? <a href="#">Visit the homepage</a> or read our <a
                         href="#">guidelines</a>.</p>
